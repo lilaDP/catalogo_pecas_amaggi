599678ba8dfdfb1fbd960c99195663383fd8f7ee
Dalila <dalilapelegrinello@outlook.com>
2026-07-02 15:36:09 -0300

    Adicionado script de migracao automatica no urls.py

--- catalogo_pecas/urls.py
+++ catalogo_pecas/urls.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.core.management import call_command
+
+# --- TRUQUE PARA CRIAR AS TABELAS AUTOMATICAMENTE NA VERCEL ---
+try:
+    print("Criando tabelas no banco temporario da Vercel...")
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Erro ao rodar as migracoes: {e}")
+# --------------------------------------------------------------
 
 urlpatterns = [
     path('admin/', admin.site.urls),
